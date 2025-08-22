3aad36233bb6a45dc929737c76ac29fd03fe4d6a
Anders Christensen <anders.christensen@protonmail.com>
2025-08-22 20:28:47 +0200

    Adding CI

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,7 @@
 import numpy as np
 from time import time
 import kernelforge as kf
+import pytest
 
 def test_inverse_distance_shapes():
     X = np.random.rand(5, 3)
@@ -9,6 +10,47 @@ def test_inverse_distance_shapes():
 
 
 def test_kernel_simple_runs():
+    rep, n = 512, 64
+    rng = np.random.default_rng(0)
+    X = np.asfortranarray(rng.random((rep, n)))
+    alpha = -1.0 / rep
+
+    K = kf.kernel_symm_simple(X, alpha)
+    assert K.shape == (n, n)
+
+    # Symmetrize since only upper triangle is written
+    iu = np.triu_indices(n, 1)
+    K[(iu[1], iu[0])] = K[iu]
+    # Check diagonal ~ 1.0
+    assert np.allclose(np.diag(K), 1.0)
+
+    # Off-diagonal entries should be between 0 and 1
+    off_diag = K[iu]
+    assert np.all((off_diag >= 0.0) & (off_diag <= 1.0))
+
+
+def test_kernel_blas_runs():
+    rep, n = 512, 64
+    rng = np.random.default_rng(0)
+    X = np.asfortranarray(rng.random((rep, n)))
+    alpha = -1.0 / rep
+
+    K = kf.kernel_symm_blas(X, alpha)
+    assert K.shape == (n, n)
+
+    # Symmetrize since only upper triangle is written
+    iu = np.triu_indices(n, 1)
+    K[(iu[1], iu[0])] = K[iu]
+
+    # Check diagonal ~ 1.0
+    assert np.allclose(np.diag(K), 1.0)
+
+    # Off-diagonal entries should be between 0 and 1
+    off_diag = K[iu]
+    assert np.all((off_diag >= 0.0) & (off_diag <= 1.0))
+
+@pytest.mark.slow
+def test_kernel_simple_time():
     rep, n = 512, 16000
     rng = np.random.default_rng(0)
     X = np.asfortranarray(rng.random((rep, n)))
@@ -30,7 +72,9 @@ def test_kernel_simple_runs():
     off_diag = K[iu]
     assert np.all((off_diag >= 0.0) & (off_diag <= 1.0))
 
-def test_kernel_blas_runs():
+
+@pytest.mark.slow
+def test_kernel_blas_time():
     rep, n = 512, 16000
     rng = np.random.default_rng(0)
     X = np.asfortranarray(rng.random((rep, n)))
